759a7270f45b29944a217cdd6d9b915fb6c5c108
spcornelius <spcornelius@gmail.com>
2018-06-27 16:05:23 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+
+
+pkg_name = 'contextutil'
+url = 'https://github.com/spcornelius/' + pkg_name
+license = 'GPLv3'
+version = '0.1.0'
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics'
+]
+
+
+def _path_under_setup(*args):
+    return os.path.join(os.path.dirname(__file__), *args)
+
+
+_author, _author_email = open(_path_under_setup('AUTHORS'), 'rt').readline().split('<')
+
+setup_kwargs = dict(
+    name=pkg_name,
+    version=version,
+    description="Some handy Python context managers",
+    classifiers=classifiers,
+    author=_author,
+    author_email=_author_email.split('>')[0].strip(),
+    url=url,
+    license=license,
+    packages=[pkg_name]
+)
+
+if __name__ == '__main__':
+    setup(**setup_kwargs)
